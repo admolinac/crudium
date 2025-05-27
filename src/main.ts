80b46cd75398949f1ea8c3b5b4916075f78d82e3
Angie Molina <amolina1795@gmail.com>
2025-05-27 15:37:12 -0500

    Init project

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap';
+import './style.css';
+
+import initApp from './utils/handleInitApp';
+
+document.addEventListener("DOMContentLoaded", initApp);
